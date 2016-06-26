fe5c2c13f4f5f4aa7093b3cfa46f2ef391fbb419
ramkpanchal <ramkpanchal@gmail.com>
2016-06-26 09:49:06 -0700

    changes

--- Sukanta1/urls.py
+++ Sukanta1/urls.py
@@ -13,10 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
-from django.contrib import admin
-
-urlpatterns = from django.conf.urls import patterns, include, url
+from django.conf.urls import patterns, include, url
 from login.views import *
 from django.contrib import admin
 
