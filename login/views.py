23b21167e0275cc22dec5cf800e5ee2bd2bba37c
ramkpanchal <ramkpanchal@gmail.com>
2016-06-28 09:06:47 -0700

    new changes

--- login/views.py
+++ login/views.py
@@ -9,28 +9,40 @@ from django.template import RequestContext
 
 @csrf_protect
 def register(request):
-	if request.method == "post":
-		form = RegistrationForm(request.POST)
-	if form.is_valid():
-		user = user.object.create.user(
-		username = form.cleaned_data['username'],
-		email = form.cleaned_data['email'])
-		return HttpResponseRedirect('/register/success/')
-	else:
-		form = RegistrationForm()
-		variables = RequestContext(request,{'form'},form)
-	return render_to_resonse('registration/register.html',
-	variables,
-)
+	  if request.method == 'POST':
+	  	 form = RegistrationForm(request.POST)
+	  	 print request.POST['username']
+	  	 if form.is_valid():
+	  	 	user = User.objects.create_user(
+	  	 	username=form.cleaned_data['username'],
+	  	 	password=form.cleaned_data['password1'],
+	  	 	email=form.cleaned_data['email']
+	  	 	)
+	  	 	return HttpResponseRedirect('/register/success/')
+	  else:
+	  	  form = RegistrationForm()
+	  variables = RequestContext(request,{
+	  'form': form
+	  	})
+
+	  return render_to_response(
+	  'registration/register.html',
+	  variables,
+	  )
 
 def register_success(request):
-	return render_to_response('registration/success',)
+	  return render_to_response(
+	  'registration/success.html',
+	  )
 
 def logout_page(request):
-	logout(request)
-	return HttpResponseRedirect('/')
+	  logout(request)
+	  return HttpResponseRedirect('/')
 
 @login_required
 def home(request):
-	return render_to_response('home.html',{'user': request.user})
+	  return render_to_response(
+	  'home.html',
+	  {  'user': request.user }
+	  )
 # Create your views here.
